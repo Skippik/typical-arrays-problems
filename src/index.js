d7b34a55c343954bc8cdc4a4497a27b3d777e839
Evgenii <Skippik1111@gmail.com>
2021-07-30 13:29:02 +0300

    feat: function Typical Arrays Problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
+
+  if(Array.isArray(array) && array.length) {
+    return Math.min(...array);
+    } else {
   return 0;
+  }
 }
 
 exports.max = function max (array) {
+
+  if(Array.isArray(array) && array.length) {
+    return Math.max(...array);
+    } else {
   return 0;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (Array.isArray(array) && array.length) {
+    let average = (accumulator, curentValue) => (accumulator + curentValue);
+    return array.reduce(average) / array.length;
+  } else {
+    return 0;
+  }
 }
